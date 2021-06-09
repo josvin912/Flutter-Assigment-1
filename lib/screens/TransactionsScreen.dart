143a09f6b7da7b1afd3b639031a836e4e36b6c59
josvin912 <69499507+josvin912@users.noreply.github.com>
2021-06-09 12:24:17 +0530

    Update TransactionsScreen.dart

--- lib/screens/TransactionsScreen.dart
+++ lib/screens/TransactionsScreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class TransactionsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Column(
       children: <Widget>[
         Padding(
